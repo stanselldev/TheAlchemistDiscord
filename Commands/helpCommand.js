1b9feae5a2391c47612216c03f2878e366250cf5
xdrewP <47803811+xdrewP@users.noreply.github.com>
2019-08-17 23:03:43 -0400

    Update helpCommand.js

--- Commands/helpCommand.js
+++ Commands/helpCommand.js
@@ -3,7 +3,20 @@ export function helpCommand(args, msg) {
       ...\n
       ***GETTING STARTED***
       \`\`\`
-      TODO: FILL THIS FORM OUT!
+      Use '!account create' to get started. 
+
+      \nOnce your account is created, it's time to get to work. A good place to begin is having a look around. Try '!explore' to see what's nearby.
+
+      \nHere's a list of all currently available commands and what they do:
+
+      \n!help - Gives this menu as a private message
+      !patch - Gives information on the latest patch version as a private message
+
+      \n!account create - create a new account
+      !account delete - delete your account
+      !account view - see your account status and info
+
+      \n!explore - displays areas available to explore
       \`\`\`
     `)
 }
