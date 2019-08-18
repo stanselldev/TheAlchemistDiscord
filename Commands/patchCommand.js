8f9f3463781c5961bd0ba367eb480c9dbce87566
xdrewP <47803811+xdrewP@users.noreply.github.com>
2019-08-17 22:49:54 -0400

    Create patchCommand.js

---
+++ Commands/patchCommand.js
@@ -0,0 +1,12 @@
+// Shows latest patch, numerical version formatted as phase.conceptVersion.update 
+// (0.1.1 means creation phase, 1st concept, 1st version)
+
+export function patchCommand(args, msg) {
+    msg.author.send(`
+      ...\n
+      **VERSION 0.0.1**
+      \n-Accounts can now be created and deleted.
+      \n-List of areas available to explore (based on your current level) can be displayed.
+      \n-Resources available in areas 1, 2, and 3 added     
+    `)
+}
